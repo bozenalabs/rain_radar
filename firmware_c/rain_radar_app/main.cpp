e0c60900751537d6c1b3f12141ba7d2674abb1f7
bozenalabs <238575338+bozenalabs@users.noreply.github.com>
2025-10-26 14:39:20 +0000

    still working

--- firmware_c/rain_radar_app/main.cpp
+++ firmware_c/rain_radar_app/main.cpp
@@ -82,6 +82,69 @@ int main() {
     };
     inky_frame.rtc.set_datetime(&dt);
 
+
+
+
+    persistent::PersistentData payload = persistent::read();
+
+    // auto on_error = [&](const std::string_view &msg, Err err)
+    // {
+    //     std::string error_msg = std::string(msg) + " (" + std::string(errToString(err)) + ")";
+    //     printf("Error: %s\n", error_msg.c_str());
+    //     draw_error(inky_frame, error_msg);
+    //     inky_frame.update(true);
+    // };
+
+    // ResultOr<int8_t> new_preferred_ssid_index = wifi_setup::wifi_connect(inky_frame, payload.wifi_preferred_ssid_index);
+    // if (!new_preferred_ssid_index.ok())
+    // {
+    //     on_error("WiFi connect failed", new_preferred_ssid_index.result);
+    //     return -1;
+    // }
+    // int8_t connected_ssid_index = new_preferred_ssid_index.unwrap();
+    // if (connected_ssid_index != payload.wifi_preferred_ssid_index)
+    // {
+    //     payload.wifi_preferred_ssid_index = connected_ssid_index;
+    //     printf("New preferred SSID index: %d\n", payload.wifi_preferred_ssid_index);
+    //     persistent::save(&payload);
+    // }
+
+    // Initialize battery monitoring
+    // MUST BE INITIALIZED AFTER WIFI SETUP ON PICO W
+    // for some reason it needs cyw43_arch_init() to have been called first
+    // Battery battery;
+    // battery.init();
+    // printf("Battery status: %s\n", battery.get_status_string());
+    // printf("%s", battery.is_usb_powered() ? "USB powered\n" : "Battery powered\n");
+
+    inky_frame.set_pen(Inky73::GREEN);
+    inky_frame.clear();
+
+    // fetching the image will write to the PSRAM display directly
+    // Err result = data_fetching::fetch_image(inky_frame, connected_ssid_index);
+    // if (result != Err::OK)
+    // {
+    //     on_error("Image fetch failed", result);
+    //     return -1;
+    // }
+
+    // points of interest
+    for (const auto &poi : secrets::POINTS_OF_INTEREST_XY)
+    {
+        inky_frame.set_pen(Inky73::WHITE);
+        inky_frame.circle(Point(poi[0], poi[1]), 3);
+        inky_frame.set_pen(Inky73::RED);
+        inky_frame.circle(Point(poi[0], poi[1]), 2);
+    }
+
+    // draw_battery_status(inky_frame, battery);
+
+    // wifi_setup::network_deinit(inky_frame);
+
+    inky_frame.update(true);
+
+
+
     inky_frame.led(InkyFrame::LED_ACTIVITY, 100);
     sleep_ms(500);
     inky_frame.led(InkyFrame::LED_ACTIVITY, 0);
@@ -98,7 +161,7 @@ int main() {
 
     printf("done!\n");
 
-    inky_frame.sleep_until(-1, 1,   -1, -1);
+    inky_frame.sleep_until(-1, 1, -1, -1);
 
     return 0;
 }
